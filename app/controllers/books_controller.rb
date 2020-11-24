08af3f9a9199408aaef27d4e9026d902a3dab303
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-11-24 17:11:06 +0100

    add resources book, books controller,pundit book policy,

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'books/create'
+  get 'books/update'
+  get 'books/destroy'
+  get 'books/new'
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
   devise_for :users
   root to: 'pages#home'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
